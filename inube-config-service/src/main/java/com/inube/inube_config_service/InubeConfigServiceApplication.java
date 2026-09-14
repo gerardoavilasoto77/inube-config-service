61419aa54b18fe736c0647654d77764cd56ee4de
gerardoavilasoto77 <gerardoavilasoto6@gmail.com>
2026-09-14 13:25:28 -0600

    Commit inicial limpio

---
+++ inube-config-service/src/main/java/com/inube/inube_config_service/InubeConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.inube.inube_config_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class InubeConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InubeConfigServiceApplication.class, args);
+	}
+
+}
---
+++ inube-config-service/src/test/java/com/inube/inube_config_service/InubeConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inube.inube_config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InubeConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
